c0d712a4e6335acc9c62269c0ba2f926aba11672
Doug Losey <douglosey@Dougs-MacBook-Pro.local>
2018-08-30 01:15:41 -0400

    done!!

--- coalesceEddie.py
+++ coalesceEddie.py
@@ -7,15 +7,14 @@ for filename in glob.glob('*.replay'):
     json_data = open(filename, "r")
     print "Processing " + filename
     if (i > 0):
-        print "if 2"
         parsed_json2 = json.load(json_data)
         parsed_json["Conversations"].append(parsed_json2["Conversations"][0])  # add 1st element from array
     if (i == 0):
         parsed_json = json.load(json_data)
         i+=1
-        print "if 1"
 
 print json.dumps(parsed_json) #collapsed json
 
-f = open("output", "w")
+f = open("output.replay", "w")
 f.write(json.dumps(parsed_json, indent=4))
+print "Check output.replay for a prettier version :p"
